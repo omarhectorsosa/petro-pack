63c436a1d60b4411734c5cf3d088a732f94726e9
ososa <omarhectorsosa@gmail.com>
2017-04-08 23:15:13 -0300

    Se cambio el Menu

--- src/PetroPack/MenuBundle/Controller/MenuController.php
+++ src/PetroPack/MenuBundle/Controller/MenuController.php
@@ -14,7 +14,7 @@ class MenuController extends Controller
     {
 
         $menues[] = array("nombre"=>"Cliente","ref"=>"/backend/cliente/list");
-        $menues[] = array("nombre"=>"Producto","ref"=>"/backend/producto/list/page/0");
+        $menues[] = array("nombre"=>"Producto","ref"=>"/backend/producto/list/page/1");
         $menues[] = array("nombre"=>"Venta","ref"=>"/backend/new");
         $venta[]   = array("nombre"=>"Crear","ref"=>"/backend/new");
         $venta[]   = array("nombre"=>"Listar","ref"=>"/backend/venta/list");
